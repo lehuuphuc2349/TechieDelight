0471589f78c513de3f47964c57be27aacdcf6c73
lehuuphuc2349 <57623623+lehuuphuc2349@users.noreply.github.com>
2020-11-09 09:48:29 +0700

    commit

--- array/src/ex14.java
+++ array/src/ex14.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ex14 {
+    static int countPrev(int arr[],int i){
+        int sum = 0;
+        for(int n=0; n<=i; n++){
+           sum+=arr[n];
+        }
+        return sum;
+    }
+    static int countNext(int arr[],int i){
+        int sum = 0;
+        for(int n = i; n < arr.length; n++){
+            sum+=arr[n];
+        }
+        return sum;
+    }
+    static void Solve(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            if(countNext(arr,i)==countPrev(arr,i))
+                System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("INPUT SIZE OF THE ARRAY:");
+        int sz = input.nextInt();
+        System.out.println("INPUT ELEMENT OF THE ARRAY:");
+        int arr[]=new int[sz];
+        for(int i=0; i<sz; i++){
+            arr[i]= input.nextInt();
+        }
+        Solve(arr);
+    }
 }
